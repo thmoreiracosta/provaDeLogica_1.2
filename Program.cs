e945736d9b38fa76b054e4486bec7b4ba8e3a6d2
thmoreiracosta <thiagomoreiracosta@gmail.com>
2021-03-13 11:53:11 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace provaDeLogica_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercício 1.2:
+            // Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro 
+            // casas decimais conforme exemplos.
+            // Fórmula: area = pi*raio**2
+            // pi = 3.14159
+
+            const double pi = 3.14159;
+            double raio, area;
+            
+            Console.WriteLine("Entre com valor do raio da circunferência: ");
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            area = pi * (raio*raio);
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
